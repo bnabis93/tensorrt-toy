d7af2f63b6e06545df4be41d52ea3937156b8471
bnabis93 <qhsh9713@gmail.com>
2022-05-10 01:40:57 +0900

    Add tensorrt fp32, fp16 benchmark

--- 03-torch-tensorrt-basic-usage/src/torch_tensorrt.py
+++ 03-torch-tensorrt-basic-usage/src/torch_tensorrt.py
@@ -1,54 +1,25 @@
-import json
+import argparse
 import time
 
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 import torchvision.models as models
-from PIL import Image
-from torchvision import transforms
 
+import torch_tensorrt
+
+parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
+parser.add_argument(
+    "--batch_size", default=1, type=int, help="Benchmark batchsize",
+)
+parser.add_argument(
+    "--num_run", default=1000, type=int, help="Number of run in benchmark.",
+)
+parser.add_argument(
+    "--quant", default="fp32", type=str, help="Set the quantization.",
+)
+args = parser.parse_args()
 cudnn.benchmark = True
-
-
-def rn50_preprocess():
-    """Preprocess module."""
-    preprocess = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    return preprocess
-
-
-def predict(img_path, model, description):
-    """Predict module.
-    decode the results into ([predicted class, description], probability)."""
-    img = Image.open(img_path)
-    preprocess = rn50_preprocess()
-    input_tensor = preprocess(img)
-    input_batch = input_tensor.unsqueeze(
-        0
-    )  # create a mini-batch as expected by the model
-
-    # move the input and model to GPU for speed if available
-    if torch.cuda.is_available():
-        input_batch = input_batch.to("cuda")
-        model.to("cuda")
-
-    with torch.no_grad():
-        output = model(input_batch)
-        # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-        sm_output = torch.nn.functional.softmax(output[0], dim=0)
-
-    ind = torch.argmax(sm_output)
-    return (
-        description[str(ind.item())],
-        sm_output[ind],
-    )  # ([predicted class, description], probability)
 
 
 def benchmark(
@@ -87,14 +58,47 @@ def benchmark(
 
 def main():
     """Main."""
-    for i in range(3):
-        img_path = "./data/img%d.JPG" % i
-        resnet50 = models.resnet50(pretrained=True)
-        resnet50.eval()
-        with open("./data/imagenet_class_index.json") as json_file:
-            description = json.load(json_file)
-        pred, prob = predict(img_path, resnet50, description)
-        print("{} - Predicted: {}, Probablility: {}".format(img_path, pred, prob))
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    resnet50 = models.resnet50(pretrained=True)
+    resnet50.eval().to(device)
+    print("Torch benchmark")
+    # benchmark(
+    #     model=resnet50, input_shape=(args.batch_size, 3, 224, 224), nruns=args.num_run,
+    # )
+    if args.quant == "fp32":
+        print("TensorRT FP32 benchmark")
+        trt_model_fp32 = torch_tensorrt.compile(
+            resnet50,
+            inputs=[
+                torch_tensorrt.Input(
+                    (args.batch_size, 3, 224, 224), dtype=torch.float32
+                )
+            ],
+            enabled_precisions=torch.float32,  # Run with FP32
+            workspace_size=1 << 22,
+        )
+        benchmark(
+            model=trt_model_fp32,
+            input_shape=(args.batch_size, 3, 224, 224),
+            nruns=args.num_run,
+        )
+    elif args.quant == "fp16":
+        print("TensorRT FP16 benchmark")
+        trt_model_fp16 = torch_tensorrt.compile(
+            resnet50,
+            inputs=[
+                torch_tensorrt.Input((args.batch_size, 3, 224, 224), dtype=torch.half)
+            ],
+            enabled_precisions={torch.half},  # Run with FP32
+            workspace_size=1 << 22,
+        )
+        benchmark(
+            model=trt_model_fp16,
+            input_shape=(args.batch_size, 3, 224, 224),
+            nruns=args.num_run,
+        )
+    else:
+        ValueError("Unsupported quantization type.")
 
 
 if __name__ == "__main__":
